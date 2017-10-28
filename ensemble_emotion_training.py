f43d19cf8e27ed506aa24344f522aae644a91a1a
Dhaval Thakkar <dhavalthakkar4995@gmail.com>
2017-10-29 05:16:35 +0530

    Update ensemble_emotion_training.py

--- ensemble_emotion_training.py
+++ ensemble_emotion_training.py
@@ -42,14 +42,6 @@ def parse_audio_files(path):
     return np.array(features), np.array(labels, dtype=np.int)
 
 
-def one_hot_encode(labels):
-    n_labels = len(labels)
-    n_unique_labels = len(np.unique(labels))
-    one_hot_encode = np.zeros((n_labels, n_unique_labels))
-    one_hot_encode[np.arange(n_labels), labels] = 1
-    return one_hot_encode
-
-
 tr_features, tr_labels = parse_audio_files('./train_sounds/*.wav')
 
 tr_features = np.array(tr_features, dtype=pd.Series)
